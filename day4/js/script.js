da50ce067001e757df10bde1c09ea7f2cfb8a01c
Sagardeepsingh21 <sagardeepsingh439@gmail.com>
2025-08-29 11:21:37 +0530

    day5

--- day4/js/script.js
+++ day4/js/script.js
@@ -1,2 +1,72 @@
 console.log("Hello Simmi");
 console.log("Hello");
+var fullName ="admin";
+console.log(fullName);
+console.log(typeof(fullName));
+
+var email="admin@gmail.com";
+console.log(email);
+console.log(typeof(email));
+
+var age=25;
+console.log(age);
+console.log(typeof(age));
+
+var choice=true;
+console.log(choice);
+console.log(typeof(choice));
+
+var mobile;
+console.log(mobile);
+console.log(typeof(mobile));
+var n1=20;
+var n2=30;
+
+var add =n1+n2;
+console.log("add = "+add);
+
+var sub=n1-n2;
+console.log("sub = "+sub);
+
+var product=n1*n2;
+console.log("priduct = "+product);
+
+var div=n1/n2;
+console.log("div = "+div);
+
+var modulo=n1%n2;
+console.log("modulo = "+modulo);
+
+var square=n1*n1;
+console.log("Square is = "+square);
+
+var cube=n1*n1*n1;
+console.log("cube is = "+cube);
+
+var n3=30;
+var n4=40;
+var n5=23;
+
+var avg=(n1+n2+n3+n4+n5)/5;
+console.log("The avg is = "+ avg);
+
+var unitprice=400;
+var quantity=5;
+var amount=unitprice*quantity;
+console.log("The amount is = "+amount);
+var discount=amount*(10/100);
+console.log("the discount is = "+discount)
+var finalAmount=amount-discount;
+console.log("the final payment after dicount is = "+finalAmount);
+
+console.log(10>5);
+console.log(15<10);
+console.log(90==90);
+console.log(90!=70);
+console.log(30>=30);
+
+var num=10;
+if(num>5){
+    console.log("number is greater than 5")
+}
+console.log("Good day !")
